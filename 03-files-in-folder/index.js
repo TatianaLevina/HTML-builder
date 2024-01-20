4b09f92510d121c3c5bfc8ee3fe326e7dbb74729
Tatiana Levina <tatiana.levina.v@gmail.com>
2024-01-20 19:21:42 +0300

    feat: add info about files in secret folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const path = require('path');
+
+const nameFile = 'secret-folder';
+const absolutePath = path.join(__dirname, nameFile);
+
+fs.readdir(absolutePath, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  files.forEach(async (file) => {
+    if (file.isFile()) {
+      const stat = await fsPromises.stat(path.join(absolutePath, file.name));
+      const fileSize = stat.size / 1024;
+      console.log(
+        `${path.basename(
+          `${file.name}`,
+          path.extname(`${file.name}`),
+        )} - ${path.extname(`${file.name}`)} - ${fileSize}kb`,
+      );
+    }
+  });
+});
